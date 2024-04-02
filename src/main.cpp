d0b926034b6a84409d510faafda1a8b090fab5ee
shkwon98 <seonghyeon.kwon@avikus.ai>
2024-04-02 12:39:54 +0900

    Add SHA3Encoder class for SHA3 hashing

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 // standard headers
+#include <bitset>
 #include <fstream>
 #include <future>
 #include <iomanip>
 #include <iostream>
+#include <set>
 #include <sstream>
 #include <string>
 #include <thread>
@@ -10,10 +12,41 @@
 #include <vector>
 
 // Crypto++ headers
+#include <cryptopp/base64.h>
 #include <cryptopp/cryptlib.h>
 #include <cryptopp/filters.h>
-#include <cryptopp/hex.h>
-#include <cryptopp/sha3.h>
+
+#include "pwdgen/sha3_encoder.h"
+
+std::array<char, 64> constexpr PasswordTable = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N',
+                                                 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b',
+                                                 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p',
+                                                 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '2', '3', '4',
+                                                 '5', '6', '7', '8', '9', '+', '-', '%', '$', '*', '<', '>' };
+
+std::vector<int> primeNumbers = { 2,   3,   5,   7,   11,  13,  17,  19,  23,  29,  31,  37,  41,  43,  47,  53,
+                                  59,  61,  67,  71,  73,  79,  83,  89,  97,  101, 103, 107, 109, 113, 127, 131,
+                                  137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223 };
+
+// Get hex string and convert it into bit vector
+std::vector<bool> HexToBits(const std::string &hex_string)
+{
+    auto bit_vector = std::vector<bool>();
+    bit_vector.reserve(hex_string.size() * 4);
+
+    for (auto i = 0U; i < hex_string.size(); i += 2)
+    {
+        auto byte = std::stoi(hex_string.substr(i, 2), nullptr, 16);
+        auto byte_bits = std::bitset<8>(byte);
+
+        for (auto j = 0U; j < byte_bits.size(); ++j)
+        {
+            bit_vector.push_back(byte_bits.test(j));
+        }
+    }
+
+    return bit_vector;
+}
 
 template <typename T>
 std::string IntToHexString(T i)
@@ -33,7 +66,39 @@ const std::string RowString(const uint32_t &mac, const std::string &key, const s
     return ss.str();
 }
 
-std::stringstream CalculateHashes(uint32_t start, uint32_t end, const std::string &key)
+template <size_t N>
+std::string ToBase64(std::bitset<N> &value)
+{
+    std::string result;
+
+    for (auto i = 0U; i < N; i += 6)
+    {
+        auto index = 0;
+        for (auto j = 0; j < 6; ++j)
+        {
+            index <<= 1;
+            index |= value[i + j];
+        }
+
+        result.push_back(PasswordTable[index]);
+    }
+
+    return result;
+}
+
+template <size_t N>
+std::bitset<N> ExtractBits(const std::bitset<256> &sha256_bitset)
+{
+    auto bitset = std::bitset<N>();
+    for (auto i = 0U; i < N; ++i)
+    {
+        bitset.set(i, sha256_bitset.test(primeNumbers[i]));
+    }
+
+    return bitset;
+}
+
+std::stringstream CalculateHashes(uint32_t start, uint32_t end, const std::string &key, std::vector<std::string> &passwords)
 {
     std::stringstream ss;
 
@@ -41,17 +106,18 @@ std::stringstream CalculateHashes(uint32_t start, uint32_t end, const std::strin
     {
         const std::string mac_with_key = std::to_string(i) + key;
 
-        CryptoPP::SHA3_256 hash;
-        CryptoPP::byte digest[CryptoPP::SHA3_256::DIGESTSIZE];
+        SHA3Encoder hash_encoder(mac_with_key);
 
-        hash.CalculateDigest(digest, reinterpret_cast<const CryptoPP::byte *>(mac_with_key.c_str()), mac_with_key.length());
+        const auto &digest = hash_encoder.GetDigest();
+        const auto &hash_string = hash_encoder.GetHexString();
+        const auto &hash_bitset = hash_encoder.GetBitset();
 
-        std::string output;
-        CryptoPP::HexEncoder encoder(new CryptoPP::StringSink(output));
-        encoder.Put(digest, sizeof(digest));
-        encoder.MessageEnd();
+        auto password_bitset = ExtractBits<48>(hash_bitset);
+        const auto &password = ToBase64(password_bitset);
 
-        ss << RowString(i, key, output, "-") << std::endl;
+        passwords.push_back(password);
+
+        ss << RowString(i, key, hash_string, password) << std::endl;
     }
 
     return ss;
@@ -61,7 +127,7 @@ int main()
 {
     auto thread_no = std::thread::hardware_concurrency();
 
-    const auto &max_mac = 0x0FFFFFU;
+    const auto &max_mac = 0xFFFFFFU;
     const auto &range = max_mac / thread_no;
     auto left_over = max_mac % thread_no;
 
@@ -70,15 +136,18 @@ int main()
 
     auto futures = std::vector<std::future<std::stringstream>>();
     auto threads = std::vector<std::thread>();
+    auto password_lists = std::vector<std::vector<std::string>>(thread_no);
 
     auto key = std::string{ getpass("Enter a key: ") };
 
     for (auto i = 0U; i < thread_no; ++i)
     {
-        auto task = std::packaged_task<std::stringstream(uint32_t, uint32_t, std::string &)>(CalculateHashes);
+        auto task = std::packaged_task<std::stringstream(uint32_t, uint32_t, std::string &, std::vector<std::string> &)>(
+            CalculateHashes);
 
         futures.emplace_back(task.get_future());
-        threads.emplace_back(std::thread(std::move(task), start_mac, end_mac, std::ref(key)));
+        password_lists[i].reserve(end_mac - start_mac + 1);
+        threads.emplace_back(std::thread(std::move(task), start_mac, end_mac, std::ref(key), std::ref(password_lists[i])));
 
         start_mac = end_mac + 1;
         end_mac += range;
@@ -93,6 +162,21 @@ int main()
     {
         thread.join();
     }
+
+    std::set<std::string> password_set;
+    uint32_t duplicate_count = 0;
+    for (const auto &passwords : password_lists)
+    {
+        for (const auto &password : passwords)
+        {
+            if (!password_set.insert(password).second)
+            {
+                ++duplicate_count;
+            }
+        }
+    }
+
+    std::cout << "Duplicate password count: " << duplicate_count << std::endl;
 
     // 결과 파일에 출력
     auto file = std::ofstream("hashes.txt");
